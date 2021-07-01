147214d5234d80d43d0cc8b1e8659a8e64f35a59
techchrism <chris@techchrism.me>
2021-06-30 21:29:36 -0700

    add websocket watching for party and match ids

--- src/info-providers/LocalInfoProvider.js
+++ src/info-providers/LocalInfoProvider.js
@@ -4,12 +4,15 @@ const fetch = require('node-fetch');
 const https = require('https');
 const EventEmitter = require('events').EventEmitter;
 const logParser = require('../logParser');
+const WebSocket = require('ws');
 
 const localAgent = new https.Agent({
     rejectUnauthorized: false
 });
 const lockfilePath = path.join(process.env['LOCALAPPDATA'], 'Riot Games\\Riot Client\\Config\\lockfile');
 const regions = ['na','ko','eu','ap'];
+const partyPrefix = '/riot-messaging-service/v1/message/ares-parties/parties/v1/parties/';
+const matchPrePrefix = '/riot-messaging-service/v1/message/ares-pregame/pregame/v1/matches/';
 
 async function getLockfileData()
 {
@@ -27,6 +30,23 @@ async function getLocalAPI(port, password, path)
         },
         agent: localAgent
     })).json();
+}
+
+async function getMatchID(port, password, status, region, puuid)
+{
+    // Get auth data
+    const authData = await getLocalAPI(port, password, 'entitlements/v1/token');
+    
+    // Get player data
+    const playerData = await (await fetch(`https://glz-${region}-1.${region}.a.pvp.net/${status === 'PREGAME' ? 'pregame' : 'core-game'}/v1/players/${puuid}`, {
+        headers: {
+            'Authorization': 'Bearer ' + authData['accessToken'],
+            'X-Riot-Entitlements-JWT': authData['token'],
+            'User-Agent': ''
+        },
+        agent: localAgent
+    })).json();
+    return playerData['MatchID'];
 }
 
 async function asyncTimeout(delay)
@@ -80,6 +100,40 @@ async function loadLocalData(port, password, retryCount = 0, delay = 1500)
     throw new Error('Retry limit reached');
 }
 
+async function waitForPrivatePresence(port, password, puuid, retryCount = -1, delay = 1500)
+{
+    for(let tries = 0; retryCount === -1 || tries <= retryCount; tries++)
+    {
+        try
+        {
+            const presenceData = await getLocalAPI(port, password, 'chat/v4/presences');
+            return JSON.parse(Buffer.from(presenceData.presences.find(p => p.puuid === puuid).private, 'base64').toString());
+        }
+        catch(e)
+        {
+            if(retryCount === -1 || tries < retryCount)
+            {
+                await asyncTimeout(delay);
+            }
+        }
+    }
+    throw new Error('Retry limit reached');
+}
+
+function getRegionFromSessions(sessions)
+{
+    const sessionKeys = Object.keys(sessions);
+    const argStart = '-ares-deployment=';
+    for(const arg of sessions[sessionKeys[0]].launchConfiguration.arguments)
+    {
+        if(arg.startsWith(argStart))
+        {
+            return arg.substring(argStart.length);
+        }
+    }
+    return null;
+}
+
 class LocalInfoProvider extends EventEmitter
 {
     constructor()
@@ -89,6 +143,11 @@ class LocalInfoProvider extends EventEmitter
         this.localData = null;
         this.clientVersion = null;
         this.region = null;
+        this.matchID = null;
+        this.matchState = null;
+        this.partyID = null;
+        
+        this.ws = null;
         
         this.watcher = fs.watch(path.dirname(lockfilePath), async (eventType, fileName) =>
         {
@@ -104,12 +163,77 @@ class LocalInfoProvider extends EventEmitter
     {
         try
         {
+            // Load lockfile data and local api endpoint (for region)
             this.lockFileData = await getLockfileData();
             this.localData = await loadLocalData(this.lockFileData.port, this.lockFileData.password);
+            this.region = getRegionFromSessions(this.localData.externalSessions);
+            
+            // Get party status and match status
+            const privatePresenceData = await waitForPrivatePresence(this.lockFileData.port, this.lockFileData.password, this.localData.sessionData['puuid']);
+            console.log(privatePresenceData);
+            
+            this.partyID = privatePresenceData['partyId'];
+            this.matchState = privatePresenceData['sessionLoopState'];
+            
+            // If a match is currently happening, get the match id
+            if(['INGAME', 'PREGAME'].includes(this.matchState))
+            {
+                console.log('Grabbing match id!');
+                this.matchID = await getMatchID(this.lockFileData.port, this.lockFileData.password, this.matchState, this.region, this.localData.sessionData['puuid']);
+                console.log('Match id:');
+                console.log(this.matchID);
+            }
+            
+            // Load the websocket to listen for party changes / matches starting and stopping
+            if(this.ws !== null && this.ws.readyState < 2)
+            {
+                this.ws.close();
+            }
+            this.ws = new WebSocket(`wss://riot:${this.lockFileData.password}@localhost:${this.lockFileData.port}`, {
+                rejectUnauthorized: false
+            });
+            this.ws.on('open', () =>
+            {
+                this.ws.send(JSON.stringify([5, 'OnJsonApiEvent_riot-messaging-service_v1_message']));
+                this.ws.send(JSON.stringify([5, 'OnJsonApiEvent_chat_v4_presences']));
+            });
+            this.ws.on('message', dataStr =>
+            {
+                if(dataStr.length === 0) return;
+    
+                const eventData = JSON.parse(dataStr);
+                const data = eventData[2];
+                if(eventData[1] === 'OnJsonApiEvent_riot-messaging-service_v1_message')
+                {
+                    if(data.uri.startsWith(matchPrePrefix))
+                    {
+                        this.matchID = data.uri.substring(matchPrePrefix.length);
+                    }
+                    else if(data.uri.startsWith(partyPrefix))
+                    {
+                        this.partyID = data.uri.substring(partyPrefix.length);
+                    }
+                }
+                else if(eventData[1] === 'OnJsonApiEvent_chat_v4_presences')
+                {
+                    const presence = data.data['presences'][0];
+                    if(presence.puuid !== this.localData.sessionData['puuid']) return;
+                    const privateData = JSON.parse(Buffer.from(presence['private'], 'base64').toString());
+                    
+                    this.matchState = privateData['sessionLoopState'];
+                    if(this.matchState === 'MENUS')
+                    {
+                        console.log('Clearing match id');
+                        this.matchID = null;
+                    }
+                }
+            });
+            
             this.emit('update');
         }
         catch(e)
         {
+            console.error(e);
             this.lockFileData = null;
             this.localData = null;
         }
@@ -168,17 +292,8 @@ class LocalInfoProvider extends EventEmitter
                 // Check if region is in startup params
                 if(this.localData !== null)
                 {
-                    const sessionKeys = Object.keys(this.localData.externalSessions);
-                    const argStart = '-ares-deployment=';
-                    for(const arg of this.localData.externalSessions[sessionKeys[0]].launchConfiguration.arguments)
-                    {
-                        if(arg.startsWith(argStart))
-                        {
-                            this.region = arg.substring(argStart.length);
-                            await context.store.setItem('region', this.region);
-                            break;
-                        }
-                    }
+                    this.region = getRegionFromSessions(this.localData.externalSessions);
+                    await context.store.setItem('region', this.region);
                 }
                 else
                 {
--- src/localAPI.js
+++ src/localAPI.js
@@ -55,6 +55,53 @@ module.exports.templateTags = [
         {
             return 'ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9';
         }
+    },
+    {
+        name: 'pregamematch',
+        displayName: 'Pregame Match ID',
+        description: 'Match ID of the current pregame',
+        async run()
+        {
+            if(localInfoProvider.matchID === null)
+            {
+                throw new Error('Not in game');
+            }
+            if(localInfoProvider.matchState !== 'PREGAME')
+            {
+                throw new Error('Past pregame stage');
+            }
+            return localInfoProvider.matchID;
+        }
+    },
+    {
+        name: 'coregamematch',
+        displayName: 'Coregame Match ID',
+        description: 'Match ID of the current game',
+        async run()
+        {
+            if(localInfoProvider.matchID === null)
+            {
+                throw new Error('Not in game');
+            }
+            if(localInfoProvider.matchState !== 'INGAME')
+            {
+                throw new Error('Before coregame stage');
+            }
+            return localInfoProvider.matchID;
+        }
+    },
+    {
+        name: 'partyid',
+        displayName: 'Party ID',
+        description: 'Valorant current party ID',
+        async run()
+        {
+            if(localInfoProvider.partyID === null)
+            {
+                throw new Error('No ID found. Is the game running?');
+            }
+            return localInfoProvider.partyID;
+        }
     }
 ];
 
