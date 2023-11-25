65dd71a3d4c42f380c30a01875dc6403ae467f68
techchrism <chris@techchrism.me>
2023-11-25 14:28:26 -0800

    add `&scope=account%20openid` to ssid reauth from Riot client to enable region fetching

--- src/util/auth/auth-from-riot-client.ts
+++ src/util/auth/auth-from-riot-client.ts
@@ -3,7 +3,7 @@ import path from 'node:path'
 import fetch from 'node-fetch'
 
 async function ssidReauth(ssid: string) {
-    const response = await fetch('https://auth.riotgames.com/authorize?redirect_uri=https%3A%2F%2Fplayvalorant.com%2Fopt_in&client_id=play-valorant-web-prod&response_type=token%20id_token&nonce=1', {
+    const response = await fetch('https://auth.riotgames.com/authorize?redirect_uri=https%3A%2F%2Fplayvalorant.com%2Fopt_in&client_id=play-valorant-web-prod&response_type=token%20id_token&nonce=1&scope=account%20openid', {
         method: 'GET',
         redirect: 'manual',
         headers: {
